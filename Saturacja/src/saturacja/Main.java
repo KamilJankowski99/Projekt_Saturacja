faa96d5efb5e56573a000b7ab90e6f1f8a8af1c3
KamilJankowski99 <kjankowski@wow.wroclaw.pl>
2020-03-22 21:24:52 +0100

    Dodana obluga saturacji

--- Saturacja/src/saturacja/Main.java
+++ Saturacja/src/saturacja/Main.java
@@ -5,33 +5,50 @@
  */
 package saturacja;
 
+import com.sun.javafx.geom.BaseBounds;
+import com.sun.javafx.geom.transform.BaseTransform;
+import com.sun.javafx.scene.BoundsAccessor;
+import java.io.File;
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.effect.SepiaTone;
+import javafx.scene.effect.ColorAdjust;
+import javafx.scene.effect.Effect;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.stage.Window;
+
+
 
 public class Main extends Application {
+
 
     final Slider opacityLevel = new Slider(0, 1, 1);    
     final Slider sepiaTone = new Slider(0, 1, 1);
     final Slider scaling = new Slider (0.5, 1, 1);
+    final Slider saturation = new Slider(0, 1, 0.5);
+    
     final Image image  = new Image(getClass().getResourceAsStream(
         "rainbow.jpg")
     );
- 
+  
+
     final Label opacityCaption = new Label("Opacity Level:");
     final Label sepiaCaption = new Label("Sepia Tone:");
     final Label scalingCaption = new Label("Scaling Factor:");
+    final Label saturationCaption = new Label("Saturation Level:"); 
  
     final Label opacityValue = new Label(
         Double.toString(opacityLevel.getValue()));
@@ -39,24 +56,35 @@ public class Main extends Application {
         Double.toString(sepiaTone.getValue()));
     final Label scalingValue = new Label(
         Double.toString(scaling.getValue()));
+    final Label saturationValue = new Label(
+        Double.toString(saturation.getValue()));
  
-    final static Color textColor = Color.WHITE;
+    
+    
+    
+    final static Color textColor = Color.BLACK;
     final static SepiaTone sepiaEffect = new SepiaTone();
+    final static ColorAdjust saturationEffect = new ColorAdjust();
+    
+
  
     @Override
     public void start(Stage stage) {
         Group root = new Group();
-        Scene scene = new Scene(root, 600, 400);
+        Scene scene = new Scene(root, 650, 800);
         stage.setScene(scene);
         stage.setTitle("Slider Sample");
-        scene.setFill(Color.BLACK);
+        scene.setFill(Color.RED);
  
         GridPane grid = new GridPane();
         grid.setPadding(new Insets(10, 10, 10, 10));
         grid.setVgap(10);
         grid.setHgap(70);
  
-        final ImageView cappuccino = new ImageView (image);
+        ImageView cappuccino = new ImageView (image);
+
+       // cappuccino.setEffect(saturationEffect);
+        sepiaEffect.setInput(saturationEffect);
         cappuccino.setEffect(sepiaEffect);
         GridPane.setConstraints(cappuccino, 0, 0);
         GridPane.setColumnSpan(cappuccino, 3);
@@ -75,6 +103,9 @@ public class Main extends Application {
                     opacityValue.setText(String.format("%.2f", new_val));
             }
         });
+        
+      //colorAdjust.setSaturation(0.2);
+      //cappuccino.setEffect(colorAdjust);
  
         GridPane.setConstraints(opacityLevel, 1, 1);
         grid.getChildren().add(opacityLevel);
@@ -86,6 +117,16 @@ public class Main extends Application {
         sepiaCaption.setTextFill(textColor);
         GridPane.setConstraints(sepiaCaption, 0, 2);
         grid.getChildren().add(sepiaCaption);
+        
+        
+        saturationCaption.setTextFill(textColor);
+        GridPane.setConstraints(saturationCaption, 0, 4);
+        grid.getChildren().add(saturationCaption);
+        saturationValue.setTextFill(textColor);
+        GridPane.setConstraints(saturationValue, 2, 4);
+        grid.getChildren().add(saturationValue);
+        GridPane.setConstraints(saturation, 1, 4);
+        grid.getChildren().add(saturation);
  
         sepiaTone.valueProperty().addListener(new ChangeListener<Number>() {
             public void changed(ObservableValue<? extends Number> ov,
@@ -93,7 +134,20 @@ public class Main extends Application {
                     sepiaEffect.setLevel(new_val.doubleValue());
                     sepiaValue.setText(String.format("%.2f", new_val));
             }
+        
         });
+        
+        saturation.valueProperty().addListener(new ChangeListener<Number>() {
+            public void changed(ObservableValue<? extends Number> ov,
+                Number old_val, Number new_val) {
+                saturationEffect.setSaturation(new_val.doubleValue());
+  
+                   // saturationEffect.setSaturation(new_val.doubleValue());
+                    saturationValue.setText(String.format("%.2f", new_val));
+            }
+        
+        });
+        
         GridPane.setConstraints(sepiaTone, 1, 2);
         grid.getChildren().add(sepiaTone);
  
@@ -121,6 +175,8 @@ public class Main extends Application {
         grid.getChildren().add(scalingValue);
  
         stage.show();
+        
+
     }
     public static void main(String[] args) {
         launch(args);
