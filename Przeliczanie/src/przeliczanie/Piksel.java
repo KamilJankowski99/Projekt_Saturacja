560443f49f69d8ca1050182b0abeef3749bb4b50
KamilJankowski99 <kjankowski@wow.wroclaw.pl>
2020-03-29 23:26:04 +0200

    Eksperymenty z obiekt Piksel; metoda do konwersji RGB to HSV

--- Przeliczanie/src/przeliczanie/Piksel.java
+++ Przeliczanie/src/przeliczanie/Piksel.java
@@ -5,29 +5,124 @@
  */
 package przeliczanie;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Kamil
  */
+class MaxMin {
+    
+    double max(double first, double second, double third){
+        double max;
+        
+        if(first - second > 0){
+            max = first;
+        }else {
+            max = second;
+        }
+        if(third - max > 0){
+            max = third;
+        }
+        
+        return max;
+    }
+    
+    double min(double first, double second, double third){
+        double min;
+        
+        if(first - second < 0){
+            min = first;
+        }else {
+            min = second;
+        }
+        if(third - min < 0){
+            min = third;
+        }        
+        return min;
+    }
+    
+    double min(double first, double second){
+        double min;
+        
+        if(first - second < 0){
+            min = first;
+        }else {
+            min = second;   
+        }
+        return min;
+    }
+}
+
 public class Piksel {
     public int r, g, b;
+    public Double HSV_value;
+    public Double chroma;
+    public Double lightness;
+    public Double HSV_hue;
+    public Double RGB_min_component;
+    public Double HSV_saturation;
+    public Double HSL_saturation;
     public Double hue, saturationv, saturationl, v, l;
     
-    
-    Piksel rgb2hsv(Piksel punkt){
+        
+        
        
-        punkt.r = 255;
+    Piksel rgb2hsv(){
+       
         
-        return punkt;
+        MaxMin comparator = new MaxMin();
+        HSV_value = comparator.max(r, g, b);    
+        RGB_min_component = comparator.min(r, g, b);
+       
+       System.out.println("Największa wartość: " + HSV_value);
+       System.out.println("Najmniejsza wartość: " + RGB_min_component);
         
-        
-        
-        
-        
-    };
+       chroma = HSV_value - RGB_min_component;
+       lightness = (HSV_value + RGB_min_component)/2;
+       
+       System.out.println("Chroma: " + chroma);
+       System.out.println("Lightness: " + lightness);
+       
+       
+       if(chroma == 0){
+           HSV_hue = 0.0;
+           System.out.println("Hue: " + HSV_hue);
+       }else if(HSV_value == 0) {
+           HSV_hue = 60 * (0 + (g - b)/chroma);
+           System.out.println("Hue: " + HSV_hue);
+       }else if(HSV_value == g){
+           HSV_hue = 60 * (2 + (b - r)/chroma);
+           System.out.println("Hue: " + HSV_hue);
+       }else if(HSV_value == b){
+           HSV_hue = 60 * (4 + (r + g)/chroma);
+           System.out.println("Hue: " + HSV_hue);
+       }
+              
+       
+       if(HSV_value == 0){
+           HSV_saturation = 0.0;
+           System.out.println("Saturation V: " + HSV_saturation);
+       }else {
+           HSV_saturation = chroma/HSV_value;
+            System.out.println("Saturation V: " + HSV_saturation);
+       }
+       
+       if(lightness == 0){
+           HSL_saturation = 0.0;
+           System.out.println("Saturation L: " + HSL_saturation);
+       }else if(lightness == 1){
+           HSL_saturation = 0.0;
+            System.out.println("Saturation L: " + HSL_saturation);
+       }else {
+           HSL_saturation = (HSV_value - lightness) / comparator.min(lightness, 1 - lightness);                         //(RGB_min_component * (lightness) + RGB_min_component * (1 - lightness));
+            System.out.println("Saturation L: " + HSL_saturation);                
+        };
 //    rgb2hsl;
 //    hsv2rgb;
 //    hsl2rgb;
 //    hsl2hsv;
 //    hsv2hsl;
+      return this;
+    }
 }
